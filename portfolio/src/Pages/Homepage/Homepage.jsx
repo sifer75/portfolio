126dae211f01d1dce3a1eb62b7e05b8f08da823c
Taupin Fabien <taupinfabien2607@gmail.com>
2023-07-08 23:22:33 +0200

    ajout d'image de fond + changement de media

--- portfolio/src/Pages/Homepage/Homepage.jsx
+++ portfolio/src/Pages/Homepage/Homepage.jsx
@@ -5,19 +5,26 @@ import { Link } from "react-router-dom";
 function Homepage() {
   return (
     <>
-      <div className="presentation presentation2">
-        Hello, <span className="name">I’m Fabien</span>, a junior full stack
-        developer.
-        <p className="introduction">i have been using eveloping websites using Mongo DB, Express JS,
-            react JS and Node JS</p>
-        <div className="redirections">
-          <Link to="mailto:taupinfabien2607@gmail.com" className="button1">
-            <div><p>Let's Chat</p></div>
-          </Link>
-          <Link to="/portfolio" className="button2">
-            <div>Portfolio</div>
-          </Link>
+      <div className="home">
+        <div className="presentation">
+          <p>Hello, <span className="name">I’m Fabien</span>,</p>a junior full stack
+          developer.
+          <p className="introduction">
+            i create websites using Mongo DB, Express JS,
+            react JS and Node JS
+          </p>
+          <div className="redirections">
+            <Link to="mailto:taupinfabien2607@gmail.com" className="button1">
+              <div>
+                <p>Let's Chat</p>
+              </div>
+            </Link>
+            <Link to="/portfolio" className="button2">
+              <div>Portfolio</div>
+            </Link>
+          </div>
         </div>
+        <div className="overlay"></div>
       </div>
     </>
   );
